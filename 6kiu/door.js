51d7e3156cbde9bf607bdf35630597ab8377a85b
Saulo Camposi <saulocamposi@gmail.com>
2017-04-04 17:03:56 -0300

    Bugfix obstacles

--- 6kiu/door.js
+++ 6kiu/door.js
@@ -1,11 +1,11 @@
 function door(events) {
   events = events.split('');
-  console.log(events);
   var code = 0;
   var state = 0;
+  var obstacle = 0;
   var codeDoor = [];
-  for (var i = 0; i < events.length; i++) {
-    console.log(events[i]);
+  var stateRecord = [];
+  for (var i = 0; i < events.length; i++) { 
 
     if ( events[i] === '.' ) {
       if ( state == 1 ) {
@@ -28,10 +28,29 @@ function door(events) {
         console.log("Inital State: " + state);
         if (code == 5) {
           state = -1;
+          stateRecord.push(state);
           code = code > 0 ? code - 1 : code = code;
         }else if (code < 5) {
-          state = 1;
-          code = code < 5 ? code + 1 : code = 5;
+
+          console.log("Pause code minus 5");
+          console.log("Code : " + code);
+          console.log("CodeDoor :" + codeDoor);
+          console.log("StateRecord : " + stateRecord);
+          console.log("StateRecord : " + stateRecord.length);
+          console.log(stateRecord[stateRecord.length -1]);
+
+          if (obstacle == 1 && code > 0 && stateRecord[stateRecord.length - 2] == 1 ) {
+            console.log("obstacle before");
+            state = -1;
+            code = code > 0 ? code - 1 : code = code;
+            stateRecord.push(state);
+          }else {
+            console.log("No obstacle before");
+            state = 1;
+            stateRecord.push(state);
+            code = code < 5 ? code + 1 : code = 5;
+
+          }
         }
       }
       else if ( state == 1) {
@@ -39,6 +58,7 @@ function door(events) {
 
         if (code == 5) {
           state = -1;
+          stateRecord.push(state);
           code = code > 0 ? code - 1 : code = code;
         }else if (code < 5) {
           state = 0;
@@ -49,6 +69,7 @@ function door(events) {
         console.log("its been here");
         if (code == 5) {
           state = 0;
+          stateRecord.push(state);
           code = code > 0 ? code - 1 : code = code;
         }else if (code < 5) {
           state = 0;
@@ -59,11 +80,14 @@ function door(events) {
     }
 
     if (events[i] === 'O') {
+      obstacle = 1;
       if (state == 1) {
         state = -1;
+        stateRecord.push(state);
         code = code > 0 ? code - 1 : code = code;
       }else if(state == -1){
         state = 1;
+        stateRecord.push(state);
         code = code < 5 ? code + 1 : code = 5;
       }
     }
