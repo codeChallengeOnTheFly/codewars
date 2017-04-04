03df1cadfb427a7a2fc933f1076366c24bc6aaed
Saulo Camposi <saulocamposi@gmail.com>
2017-04-04 14:57:27 -0300

    Bugfix door almost complete - (fail in obstacles)

--- 6kiu/door.js
+++ 6kiu/door.js
@@ -5,47 +5,77 @@ function door(events) {
   var state = 0;
   var codeDoor = [];
   for (var i = 0; i < events.length; i++) {
+    console.log(events[i]);
 
     if ( events[i] === '.' ) {
       if ( state == 1 ) {
-        code = code--;
+        code = code < 5 ? code + 1 : code = 5;
       }
 
       if ( state == -1 ) {
-        code = code++;
+        code = code > 0 ? code - 1 : code = code;
       }
 
     }
 
-    if (events[i] === 'P') {
+    if (events[i] === "P") {
+      console.log(i);
+
+      console.log("PUSH");
+      console.log(state);
 
       if ( state == 0 ){
-        //could go to ++ or --
-        state = 1;
-        code = code++;
+        console.log("Inital State: " + state);
+        if (code == 5) {
+          state = -1;
+          code = code > 0 ? code - 1 : code = code;
+        }else if (code < 5) {
+          state = 1;
+          code = code < 5 ? code + 1 : code = 5;
+        }
       }
+      else if ( state == 1) {
+        console.log("its been here at state one");
 
-      if ( state == 1) {
-        state = 0;
-        //maintain code
+        if (code == 5) {
+          state = -1;
+          code = code > 0 ? code - 1 : code = code;
+        }else if (code < 5) {
+          state = 0;
+        }
+        console.log("State: " + state);
       }
-
-      if ( state == -1 ) {
-        state = 0;
-        code = code++;
+      else if ( state == -1 ) {
+        console.log("its been here");
+        if (code == 5) {
+          state = 0;
+          code = code > 0 ? code - 1 : code = code;
+        }else if (code < 5) {
+          state = 0;
+        }
+        console.log("State Negative" + state);
       }
+      console.log("Final State: " + state);
     }
 
     if (events[i] === 'O') {
-      state = -1;
-      code = code--;
+      if (state == 1) {
+        state = -1;
+        code = code > 0 ? code - 1 : code = code;
+      }else if(state == -1){
+        state = 1;
+        code = code < 5 ? code + 1 : code = 5;
+      }
     }
-
     codeDoor.push(code);
-
   }
 
-  return codeDoor.toString();
+  codeDoor = codeDoor.toString()
+  while (codeDoor.search(",") > 0) {
+      codeDoor = codeDoor.replace(",", "");
+  }
+  return codeDoor;
 }
 
-console.log(door("P......P......"));
+//console.log(door("P......P......"));
+console.log(door('P..OP..P..'));
