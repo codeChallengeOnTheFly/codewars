e2f0ec8c7b115b1f0db4c1cdb6b46c87f461eead
Saulo Camposi <saulocamposi@gmail.com>
2017-04-05 14:50:14 -0300

    Bugfix door 6kiu

--- 6kiu/door.js
+++ 6kiu/door.js
@@ -5,57 +5,41 @@ function door(events) {
   var obstacle = 0;
   var codeDoor = [];
   var stateRecord = [];
-  for (var i = 0; i < events.length; i++) { 
+  for (var i = 0; i < events.length; i++) {
 
     if ( events[i] === '.' ) {
       if ( state == 1 ) {
         code = code < 5 ? code + 1 : code = 5;
       }
-
       if ( state == -1 ) {
         code = code > 0 ? code - 1 : code = code;
       }
-
     }
 
     if (events[i] === "P") {
-      console.log(i);
-
-      console.log("PUSH");
-      console.log(state);
-
       if ( state == 0 ){
-        console.log("Inital State: " + state);
         if (code == 5) {
           state = -1;
           stateRecord.push(state);
           code = code > 0 ? code - 1 : code = code;
         }else if (code < 5) {
-
-          console.log("Pause code minus 5");
-          console.log("Code : " + code);
-          console.log("CodeDoor :" + codeDoor);
-          console.log("StateRecord : " + stateRecord);
-          console.log("StateRecord : " + stateRecord.length);
-          console.log(stateRecord[stateRecord.length -1]);
-
           if (obstacle == 1 && code > 0 && stateRecord[stateRecord.length - 2] == 1 ) {
-            console.log("obstacle before");
             state = -1;
             code = code > 0 ? code - 1 : code = code;
             stateRecord.push(state);
-          }else {
-            console.log("No obstacle before");
-            state = 1;
-            stateRecord.push(state);
-            code = code < 5 ? code + 1 : code = 5;
-
+          } else {
+            if(stateRecord[stateRecord.length - 1] == -1){
+              state = -1;
+              code = code > 0 ? code - 1 : code = code;
+            }else {
+              state = 1;
+              stateRecord.push(state);
+              code = code < 5 ? code + 1 : code = 5;
+            }
           }
         }
       }
       else if ( state == 1) {
-        console.log("its been here at state one");
-
         if (code == 5) {
           state = -1;
           stateRecord.push(state);
@@ -63,20 +47,16 @@ function door(events) {
         }else if (code < 5) {
           state = 0;
         }
-        console.log("State: " + state);
       }
       else if ( state == -1 ) {
-        console.log("its been here");
         if (code == 5) {
-          state = 0;
+          state = 1;
           stateRecord.push(state);
           code = code > 0 ? code - 1 : code = code;
         }else if (code < 5) {
           state = 0;
         }
-        console.log("State Negative" + state);
       }
-      console.log("Final State: " + state);
     }
 
     if (events[i] === 'O') {
